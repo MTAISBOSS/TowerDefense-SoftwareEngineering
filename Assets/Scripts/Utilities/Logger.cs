1890c47b43191fc445158b409dff77b83b82f9d1
Mohammad Taher Ahmadi <103352892+MTAISBOSS@users.noreply.github.com>
2026-05-04 23:51:18 +0330

    Added custom logger class

---
+++ Assets/Scripts/Utilities/Logger.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace Utilities
+{
+    public static class Logger
+    {
+        [Conditional("ENABLE_LOG")]
+        public static void Log(string message)
+        {
+            UnityEngine.Debug.Log(message);
+        }
+        [Conditional("ENABLE_LOG")]
+        public static void LogError(string message)
+        {
+            UnityEngine.Debug.LogError(message);
+        }
+        [Conditional("ENABLE_LOG")]
+        public static void LogWarning(string message)
+        {
+            UnityEngine.Debug.LogWarning(message);
+        }
+    }
+}
