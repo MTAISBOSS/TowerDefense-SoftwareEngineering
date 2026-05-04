baf54dd8a0a9181a497e9aab80bd5452200688cb
Mohammad Taher Ahmadi <103352892+MTAISBOSS@users.noreply.github.com>
2026-05-04 23:55:27 +0330

    Added game manager and its related event

---
+++ Assets/Scripts/Events/GameResultEvent.cs
@@ -0,0 +1,17 @@
+﻿using EventSystem;
+
+namespace Events
+{
+    public struct GameResultEvent : IEvent
+    {
+        public int totalTowers;
+        public int remainingMoney;
+        public GameState state;
+    }
+
+    public enum GameState : byte
+    {
+        Victory,
+        Defeated
+    }
+}
