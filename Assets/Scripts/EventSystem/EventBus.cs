3c224af4718ba9167469174b3f70e8611475c595
Mohammad Taher Ahmadi <103352892+MTAISBOSS@users.noreply.github.com>
2026-05-04 23:54:49 +0330

    Implemented simple event bus (#119)
    the reflection part for collecting all events still remains

---
+++ Assets/Scripts/EventSystem/EventBus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EventSystem
+{
+    public class EventBus<T> where T: IEvent
+    {
+        private static readonly HashSet<IEventBinding<T>> Bindings = new HashSet<IEventBinding<T>>();
+
+        public static void Register(EventBinding<T> binding) => Bindings.Add(binding);
+        public static void Unregister(EventBinding<T> binding) => Bindings.Remove(binding);
+
+        public static void Raise(T @event)
+        {
+            foreach (var binding in Bindings)
+            {
+                binding.OnEventNoArgs.Invoke();
+                binding.OnEvent.Invoke(@event);
+            }
+        }
+    }
+
+    public interface IEvent
+    {
+    }
+}
