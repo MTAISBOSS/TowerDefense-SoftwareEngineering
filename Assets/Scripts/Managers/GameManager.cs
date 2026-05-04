6fed7f71674e05d0c39822812d50bd0b540f4baa
Mohammad Taher Ahmadi <103352892+MTAISBOSS@users.noreply.github.com>
2026-05-05 00:06:01 +0330

    Added game manager and its related event (#120)

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Events;
+using EventSystem;
+using UnityEngine;
+
+namespace Managers
+{
+    public class GameManager : MonoBehaviour
+    {
+        private EventBinding<GameResultEvent> gameResultEvent;
+        private GameResultEvent _result;
+
+        private void OnEnable()
+        {
+            gameResultEvent = new EventBinding<GameResultEvent>(HandleGameOver);
+            EventBus<GameResultEvent>.Register(gameResultEvent);
+        }
+        private void OnDisable()
+        {
+            EventBus<GameResultEvent>.Unregister(gameResultEvent);
+        }
+        private void HandleGameOver(GameResultEvent result)
+        {
+            _result = result;
+            Invoke($"Handle{result.state.ToString()}",0);
+        }
+
+        void HandleVictory()
+        {
+            Utilities.Logger.Log($"Victory {_result}");
+        }
+        void HandleDefeated()
+        {
+            Utilities.Logger.Log("Defeated {_result}");
+        }
+    }
+}
