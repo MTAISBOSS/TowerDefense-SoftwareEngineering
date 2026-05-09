08fd3c7edbbcea6449dfb7f83dfca08cbedcfbfe
Mohammad Taher Ahmadi <103352892+MTAISBOSS@users.noreply.github.com>
2026-05-09 13:00:43 +0330

    Feature/game manager (#124)
    * Added game manager and its related event
    
    * Eventbus completed

---
+++ Assets/Scripts/EventSystem/EventBusUtil.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using Logger = Utilities.Logger;
+
+namespace EventSystem
+{
+    public static class EventBusUtil
+    {
+        public static IReadOnlyList<Type> EventTypes { get; set; }
+        public static IReadOnlyList<Type> EventBusTypes { get; set; }
+
+#if UNITY_EDITOR
+        public static PlayModeStateChange PlayModeState { get; set; }
+
+        [InitializeOnLoadMethod]
+        public static void InitializeEditor()
+        {
+            EditorApplication.playModeStateChanged -= OnPlayModeStateChanged;
+            EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+        }
+
+        private static void OnPlayModeStateChanged(PlayModeStateChange state)
+        {
+            PlayModeState = state;
+            if (state == PlayModeStateChange.ExitingPlayMode)
+            {
+                ClearAllBuses();
+            }
+        }
+#endif
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void Initialize()
+        {
+            EventTypes = PredefinedAssemblyUtil.GetTypes(typeof(IEvent));
+            EventBusTypes = InitializeAllBuses();
+        }
+
+        private static IReadOnlyList<Type> InitializeAllBuses()
+        {
+            List<Type> eventBusTypes = new List<Type>();
+
+            var typedef = typeof(EventBus<>);
+            foreach (var eventType in EventTypes)
+            {
+                var busType = typedef.MakeGenericType(eventType);
+                eventBusTypes.Add(busType);
+                Logger.Log($"Initialized EventBus<{eventType.Name}>");
+            }
+
+            return eventBusTypes;
+        }
+
+        public static void ClearAllBuses()
+        {
+            Logger.Log("Clearing all buses");
+            for (int i = 0; i < EventBusTypes.Count; i++)
+            {
+                var busType = EventBusTypes[i];
+                var clearMethod = busType.GetMethod("Clear",
+                    System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+                if (clearMethod != null) clearMethod.Invoke(null, null);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/EventSystem/IEvent.cs
@@ -0,0 +1,6 @@
+﻿namespace EventSystem
+{
+    public interface IEvent
+    {
+    }
+}
